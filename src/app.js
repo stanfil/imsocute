ffca5b8f44cc33013ba7c1080c96a978b95f3ead
lovegood <287310886@qq.com>
2017-08-07 16:17:36 +0800

    app.js commit

---
+++ src/app.js
@@ -0,0 +1,38 @@
+let express = require('express');
+let orm = require('orm');
+let app = express();
+let bodyPaser = require('body-parser');
+let path = require('path');
+let urlencodedParser = bodyPaser.urlencoded({extended: true});
+let appRoot = path.join(__dirname, '/');
+
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+app.use(orm.express(`sqlite://${appRoot}jfglxt.db`, {
+    define: function (db, models, next) {
+        models.User = db.define("user", {
+            user_name : String,
+            phoneNumber : String,
+            password : String,
+            points : Number,
+            manager : Number
+        });
+        models.Log = db.define('logs', {
+            id : Number,
+            operation : Number,
+            date : String,
+            user_pn : String,
+            manager_pn : String
+        });
+    }
+}));
+
+app.listen(8081, function () {
+    console.log("App is listening on port 8081!");
+});
