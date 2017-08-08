b18a788009b7378e1665f3ae44ea50cdb310cd95
MyunFeiYang <420222aa>
2017-08-08 09:52:01 +0800

    登录界面修改

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,12 @@
+
+
+// $(document).ready(function () {
+//     var btu1=document.getElementById('login_user_btu');
+//     var btu2=document.getElementById('login_admin_btu');
+//     btu1.onclick=change_div('login_user','login_admin');
+//     btu2.onclick=change_div('login_admin','login_user');
+//
+// });
 function change_div(div1,div2) {
     if (document.getElementById(div1).style.display=='none'){
         document.getElementById(div2).style.display='none';
@@ -6,7 +15,3 @@ function change_div(div1,div2) {
         document.getElementById(div1).style.display='none';
     }
 }
-// window.onload=function () {
-//     document.getElementById('login_user_btu').onclick=change_div('login_user','login_admin');
-//     document.getElementById('login_admin_btu').onclick=change_div('login_admin','login_user');
-// }
